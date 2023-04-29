b9da194d2af5be2487f4c788dc3f188f2e8a026b
Victor Manuel <victormalsx@gmail.com>
2023-04-29 12:55:28 -0500

    style: :wrench: Project setup and styles added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es6: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
@@ -10,6 +11,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': 'warn',
+    quotes: [0, 'single', { avoidEscape: true }],
   },
-}
+};
